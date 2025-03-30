31ecefb9c67ba0e75089f3db382eafb3e3c9fbdf
Dipanjan Das <dasdipanjan04@gmail.com>
2025-03-31 00:24:16 +0200

    Implement Markov Strategies.

---
+++ strategies/markov_strategy.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from base_strategy import BaseStrategy
+
+class MarkovStrategy(BaseStrategy):
+    def __init__(self, markov_model, state_series: pd.Series):
+        self.model = markov_model
+        self.state_series = state_series
+        self.model.fit(state_series)
+
+    def generate_signals(self, data: pd.DataFrame) -> pd.Series:
+        signals = []
+        for i in range(len(self.state_series) - 1):
+            current_state = self.state_series.iloc[i]
+            next_state = self.model.predict_next_state(current_state)
+
+            if next_state == 2:   # Predicting up
+                signals.append(1)
+            elif next_state == 0: # Predicting down
+                signals.append(-1)
+            else:
+                signals.append(0)
+        signals.append(0)
+        return pd.Series(signals, index=data.index)
